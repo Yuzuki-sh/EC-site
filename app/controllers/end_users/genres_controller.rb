610ab6eb459cb077e352d673de29ad6762b22aee
vagrant <vagrant@localhost.localdomain>
2020-09-28 12:45:10 +0000

    fixed genre search

--- app/controllers/end_users/genres_controller.rb
+++ app/controllers/end_users/genres_controller.rb
@@ -2,6 +2,6 @@ class EndUsers::GenresController < ApplicationController
     before_action :authenticate_end_user!
     def show 
         @genres = Genre.all
-        @end_user_items = current_end_user.items
+        @genre_items = Genre.find(params[:id]).items
     end
 end
