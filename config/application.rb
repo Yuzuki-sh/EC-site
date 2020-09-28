3dcaf3d8d721628e6cbb174ebc64286cb28c57ab
vagrant <vagrant@localhost.localdomain>
2020-09-28 09:50:30 +0000

    update features

--- config/application.rb
+++ config/application.rb
@@ -10,10 +10,6 @@ module NaganoCake
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-<<<<<<< HEAD
-    config.secret_key_base = 'blipblapblup'
-=======
->>>>>>> 98d3282a87177f2c5ffc136c46e08a2b9b11adbf
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
